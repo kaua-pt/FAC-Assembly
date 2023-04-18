3db6c790295510036aaf66557ab77f94c3e823e3
Kauã Vinícius <81399319+kaua-pt@users.noreply.github.com>
2023-04-18 00:15:09 -0300

    OOOOOOOOOOOOXI

--- Lista2/Bitparidade.s
+++ Lista2/Bitparidade.s
@@ -1,46 +1,34 @@
-.data
-
-.text
-main:
-    li $v0, 5
-    syscall
-    move $a0,$v0
-    j bitparidade
-
-um:
-    move $v0 $zero
-    addi $v0,$v0,1
-    addi $v1,$a0,128
-    j sVolta
-zero:
-    move $v0 $zero
-    move $v1,$a0
-    j sVolta
-
-somaUm:
-    add $t9,$t9,1 # soma ao contador
-    j volta
-
 bitparidade:
-    #a0 é meu numero padrao
-    li $t0,1 # mascara
-    move $t9,$zero #contador bits 
-    move $t4,$zero #contador loop
+    move    $t0, $zero          # cnt loop = 0
+    move    $t1, $zero          # qnt 1's = 0
+    move    $t2, $a0            # valor shift = num orig
+    move    $v1, $a0            # valor shift = num orig
+    addi    $t3, $zero, 7       # qnt loop = 7
+    addi    $t5, $zero, 2       # const = 2
+for:
+    beq     $t0, $t3, fimFor    # if cnt loop = qnt loop -> jump fimFor
 
-    loop:
-        beq $t4,7,print
-        and $t1,$t0,$a0 #faz paridade com 1
-        beq $t1,1,somaUm # se o and der um é dento
-        volta:
-        srl	$s0,$s0,1 # rigth no numero
-        addi $t4,$t4,1 #soma ao loop
-        j loop
+    andi    $t4, $t2, 1         # verifica o bit menos significativo
+    
+    beq     $t4, $zero, naoSoma # if bit menos significativo = 0 -> jump naoSoma
+    addi    $t1, $t1, 1         # qnt 1's++
+    
+naoSoma:
+    srl		$t2, $t2, 1			# $t2 = $t2 >> 1 faz o shift do valor, reduzindo um bit por vez
+    
+    addi    $t0, $t0, 1         # cnt loop ++
+    j for                       # reinicia loop
+fimFor:
+#    rem     $t6, $t1, $t5       # t6 = t2 % t5
+    div     $t1, $t5
+    mfhi    $t6
+    beq     $t6, $zero, par     # if t6 = 0 -> jump par
+    addi    $v0, $zero, 1       # v0 = 1
+    ori     $v1, $v1, 128 # altera o bit mais significativo
+    j       fimProg             # jump fim prog
+par:
+    move    $v0, $zero          # v0 = 0
+    ori     $v1, $v1, 0
+fimProg:
 
-print:
-    and $t7,$t0,$t9
-    beq $t7,0,zero
-    beq $t7,1,um
-        
-sVolta:
-    jr $ra #finaliza
-
+    jr $ra                          # retorna linha padrão do programa
